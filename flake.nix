cf183dd5b408c30f7fd620740503f9959cd21939
CelestialCrafter <celestialcraftermc@gmail.com>
2025-03-25 17:00:27 -0500

    nix build

--- flake.nix
+++ flake.nix
@@ -4,16 +4,30 @@
     { nixpkgs, ... }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
+      buildInputs = with pkgs; [
+        ninja
+        clang
+        libmpdclient
+        curl
+        tomlc99
+      ];
+    in
+    {
+      packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+        name = "lyricizer";
+        src = ./.;
+
+        inherit buildInputs;
+
+        buildPhase = "ninja";
+        installPhase = ''
+          mkdir -p $out/bin
+          cp build/lyricizer $out/bin/lyricizer
+        '';
+      };
+
       devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = with pkgs; [
-          ninja
-		  clang
-		  libmpdclient
-		  curl
-		  tomlc99
-        ];
+        packages = buildInputs;
       };
     };
 }
-
